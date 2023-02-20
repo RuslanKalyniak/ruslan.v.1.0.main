1c62bcf855a02168c71bbc4b33c56de7b5b5140c
Kalyniak Ruslan <ruslan.kalyniak@wtt-solutions.com>
2023-02-20 17:37:56 -0500

    first-commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nextjs-100-ts
+Build a PWA to achieve 100% in Google Lighthouse and Next.js Analytics 
---
+++ next.config.js
@@ -0,0 +1,57 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+const isProd = () => process.env.NODE_ENV === 'production'
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self'${!isProd() ? ' \'unsafe-eval\'' : ''};
+  connect-src 'self' vitals.vercel-insights.com;
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+`
+
+const securityHeaders = () => [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+]
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'nl'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
+  pwa: {
+    disable: !isProd(),
+    dest: 'public',
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders(),
+      },
+    ]
+  },
+})
+
+module.exports = nextConfig
